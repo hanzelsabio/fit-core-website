544c272822bf8445c522549334ac423579777543
hanzelsabio <zeluckgt01@gmail.com>
2025-11-07 02:26:31 +0800

    Added changes to Footer component

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,7 +4,7 @@ import "../../assets/styles/globals.css";
 export default function Footer() {
   return (
     <footer className="bg-white text-center py-6">
-      <p>© {new Date().getFullYear()} | Hype Plus | All rights reserved</p>
+      <p>© {new Date().getFullYear()} | Brand | All rights reserved</p>
       Designed by{" "}
       <a href="https://hanzelsabio.vercel.app/" target="_blank">
         Hanzel Sabio
