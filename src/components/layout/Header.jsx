3d0fd7966f4b18e0f7f026cd1b2478bf7236fd3e
hanzelsabio <zeluckgt01@gmail.com>
2025-11-06 20:20:36 +0800

    ..

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -69,7 +69,7 @@ export default function Header() {
         <div className="absolute left-1/2 -translate-x-1/2">
           <a href="/">
             <img
-              src="/public/brand-logo.png"
+              src="/src/assets/images/brand-logo.png"
               className="w-40"
               alt="Brand Logo"
             />
