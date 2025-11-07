8c7a714ea61c3b2ac783fc2e19cbb28ff0746268
hanzelsabio <zeluckgt01@gmail.com>
2025-11-08 00:27:31 +0800

    Added not found image and some changes to not found.

--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Hero() {
-  return <div className="bg-black min-h-[40vh]"></div>;
+  return (
+    <section className="bg-black min-h-[40vh]">
+      <div className="flex items-center justify-center item-center">
+        <h2 className="about-title text-white text-2xl font-bold uppercase mb-8">
+          HERO BANNER
+        </h2>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,10 +3,13 @@ import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
-    <section className="text-center py-12">
-      <h2 className="text-4xl font-bold mb-4 text-red-600">404</h2>
-      <p className="text-gray-600 mb-6">Page not found.</p>
-      <Link to="/" className="text-blue-600 underline">
+    <section className="flex flex-col items-center justify-center min-h-[80vh] text-center bg-gray-50">
+      {/* <h2 className="text-4xl font-bold mb-4 text-red-600">404</h2> */}
+      <img src="/not-found.png" className="w-80" alt="404" />
+      <p className="text-gray-600 mb-6">
+        The page you requested does not exist.
+      </p>
+      <Link to="/" className="underline hover:text-gray-500 transition-colors">
         Go back home
       </Link>
     </section>
