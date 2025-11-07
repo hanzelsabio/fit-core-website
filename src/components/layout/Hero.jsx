21bbbf6eaa43e266c18f452f4ad054b179f3251b
hanzelsabio <zeluckgt01@gmail.com>
2025-11-08 00:29:09 +0800

    ..

--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -4,9 +4,7 @@ function Hero() {
   return (
     <section className="bg-black min-h-[40vh]">
       <div className="flex items-center justify-center item-center">
-        <h2 className="about-title text-white text-2xl font-bold uppercase mb-8">
-          HERO BANNER
-        </h2>
+        <h2 className="about-title text-white text-2xl font-bold uppercase mb-8"></h2>
       </div>
     </section>
   );
